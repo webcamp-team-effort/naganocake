0c96922f1604778480829717db5deeb1819204e3
vagrant <vagrant@localhost.localdomain>
2020-06-17 08:42:05 +0000

    model create

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,2 +1,6 @@
 class OrderProduct < ApplicationRecord
+  belongs_to :order
+  belongs_to :product
+
+  attachment :image
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :order_products, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  belongs_to :genre
+
+  attachment :image
 end
