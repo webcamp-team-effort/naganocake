5da189f2d912c438e20084d6cb5a0caeda06a4f6
vagrant <vagrant@localhost.localdomain>
2020-06-17 12:52:06 +0000

    provisional controller

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,2 +1,31 @@
 class DeliveriesController < ApplicationController
+  # 配送先一覧＋登録画面
+  def index
+    
+  end
+
+  # 配送先登録
+  def create
+    
+  end
+
+  # 配送先編集画面（一覧画面に編集ボタンあり）
+  def edit
+    
+  end
+
+  # 配送先更新（編集画面にて更新）
+  def update
+    
+  end
+
+  # 配送先削除（一覧画面に削除ボタンあり）
+  def destroy
+    
+  end
+
+  private
+  def delivery_params
+    params.require(:delivery).permit(:customer_id, :postcode, :address, :name)
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,36 @@
 class OrdersController < ApplicationController
+  # 注文確認画面
+  def confirm
+    
+  end
+
+  # 注文完了画面
+  def finish
+    
+  end
+
+  # 注文履歴一覧（お客様単位）
+  def index
+    
+  end
+
+  # 注文履歴詳細
+  def show
+    
+  end
+
+  # 新規注文画面（支払先・配送情報画面）
+  def new
+
+  end
+
+  # 新規注文の作成
+  def create
+
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postage, :total_price, :payment_method, :send_postcode, :send_address, :send_name, :status)
+  end
 end
