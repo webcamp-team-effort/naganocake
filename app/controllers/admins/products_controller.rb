0bc5d7839031b73f1ae911251a8477e9aff052be
komi53 <kxs53kuro@outlook.jp>
2020-06-18 08:28:43 +0000

    product_new_add

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,2 +1,26 @@
 class Admins::ProductsController < ApplicationController
+	def new
+		@product = Product.new
+	end
+	def index
+		@products = Product.all
+	end
+	def show
+		
+	end
+	def edit
+		
+	end
+	def create
+		
+	end
+	def update
+		
+	end
+
+
+
+
+
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,14 @@ class ProductsController < ApplicationController
   def top
     
   end
-  
+
+  def index
+  	
+  end
+
+  def show
+  	
+  end
+
+
 end
