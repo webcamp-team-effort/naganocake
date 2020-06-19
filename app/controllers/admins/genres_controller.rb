16dd217b414c4f9282448980c0185e912d3e3cfd
komi53 <kxs53kuro@outlook.jp>
2020-06-19 09:06:00 +0000

    genre_add

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,25 @@
 class Admins::GenresController < ApplicationController
+	def index
+		@genre = Genre.new
+		@genres = Genre.all
+	end
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+	def create
+		@genre = Genre.new(genre_params)
+		@genre.save
+		redirect_to admins_genres_path
+	end
+	def update
+		@ganre = Genre.find(params[:id])
+		@ganre.update(genre_params)
+		redirect_to admins_genres_path
+	end
+
+private
+	def genre_params
+	params.require(:genre).permit(:name, :is_actived)
+	end
+
 end
--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -12,15 +12,20 @@ class Admins::ProductsController < ApplicationController
 		
 	end
 	def create
-		
+		@product = Product.new(product_params)
+		@product.save
+		redirect_to admins_products_path
 	end
 	def update
-		
+		@product = Product.find(params[:id])
+		@product.update(product_params)
+		redirect_to admins_products_path
 	end
 
 
-
-
-
-
+private
+	def product_params
+	params.require(:product).permit(:genre_id, :name, :explanation, :tax_included_price, :image, :on_sale)
+	end
 end
+
