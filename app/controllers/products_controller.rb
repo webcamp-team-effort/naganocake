76ee59eb4f3ca83640ea75a165a888d547f3ab65
komi53 <kxs53kuro@outlook.jp>
2020-06-20 14:15:14 +0000

    product_add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,22 @@
 class ProductsController < ApplicationController
   def top
-    
+    @genres = Genre.all
+	@products = Product.where(on_sale: true).page(params[:page]).per(4)
+	@product_all = Product.all
   end
 
   def index
-  	
+  	@genres = Genre.all
+	@products = Product.where(on_sale: true).page(params[:page]).per(8)
+	@product_all = Product.all
   end
 
   def show
-  	
+  	@genres = Genre.all
+  	@product = Product.find(params[:id])
+  	a = @product.tax_included_price.to_f
+	b = 1.1.to_f
+	@tax_included = "#{a*b}".to_i
   end
 
 
