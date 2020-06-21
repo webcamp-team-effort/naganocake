9469af0306240396fd09ffd8ddb4c9fd25a6d73c
vagrant <vagrant@localhost.localdomain>
2020-06-21 02:32:57 +0000

    e

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,4 +1,6 @@
 class Admins::HomesController < ApplicationController
   def top
+  	search_date = Date.today
+  	@order_count = Order.where(created_at: search_date.in_time_zone.all_day)
   end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,26 @@
 class CustomersController < ApplicationController
+	def leave
+		customer = Customer.find(params[:id])
+		customer.update(is_actived: false)
+		reset_session
+		redirect_to root_path
+	end
+	def confirm
+	end
+	def edit
+		@customer = Customer.find(params[:id])
+	end
+	def show
+		@customer = Customer.find(params[:id])
+	end
+	def update
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		redirect_to customer_path(@customer)
+	end
+
+	private
+  	def customer_params
+      params.require(:cutomer).permit(:email, :encrypted_password, :reset_password_token , :ast_name , :first_name , :last_kana_name , :first_kana_name , :postcode , :address , :phone_number , :is_actived  )
+  	end
 end
