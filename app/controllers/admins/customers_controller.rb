aa42e5a99caa5a3113ba22ff322b272cf7d92692
vagrant <vagrant@localhost.localdomain>
2020-06-23 09:59:38 +0000

    admins_customer_add

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,23 @@
 class Admins::CustomersController < ApplicationController
+	def index
+		@customers = Customer.all
+	end
+	def show
+		@customer = Customer.find(params[:id])
+	end
+	def edit
+		@customer = Customer.find(params[:id])
+	end
+	def update
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		redirect_to admins_customer_path(@customer.id)
+	end
+
+    private
+	def customer_params
+	params.require(:customer).permit(:customer_id, :name, :kana_name, :last_name, :first_name, :last_kana_name, :first_kana_name, :postcode, :address, :phone_number, :email, :is_actived)
+	end
 end
+
+
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,6 +1,4 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
-
-  attachment :image
 end
