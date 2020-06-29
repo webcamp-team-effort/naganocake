55e8a9150e828cb420caa5063988cd93474d4d8f
vagrant <vagrant@localhost.localdomain>
2020-06-29 05:53:15 +0000

    finish

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -2,6 +2,6 @@ class Admins::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
   	search_date = Date.today
-  	@orders = Order.where("created_at >= ?",search_date)
+  	@orders = Order.where("created_at >= ?", Time.zone.now.beginning_of_day)
   end
 end
